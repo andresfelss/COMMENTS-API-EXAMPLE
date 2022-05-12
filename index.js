f1e3471585e1586015f9162c1019579f13f915a2
andre <andres.felipepr17@gmial.com>
2022-05-12 11:09:40 -0500

    Config the view engine

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Importamos Express
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// Configurando ejs
+app.set('view engine','ejs'); // Recordemos que express lo importa solo
+app.set('views', path.join(__dirname,'/views')); // Definimos la ruta de mis views
+
+
+
 
 
 app.listen(3000, ()=>{
